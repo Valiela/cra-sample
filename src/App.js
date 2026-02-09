d7b06460e81051bb81ed87a93ba76d2e6f7137f1
Valiela <wallace0814@gmail.com>
2026-02-09 12:38:58 +0800

    setup

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import logo from './assets/logo.svg';
+import './assets/App.css';
+import Input from './components/Input';
+import './assets/all.scss';
+
+function App() {
+  const [text, setText] = useState('');
+
+  const onChangeHandler = (e) =>{
+    setText(e.target.value);
+  }
+
+  useEffect(()=>{
+    (async()=>{
+      const result = await axios.get('https://randomuser.me/api/');
+      console.log(result);
+    })();
+  },[])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <button type="button" className="btn btn-primary">Primary</button>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        
+        <Input id='sample' text='is text' value={text} onChangeHandler={onChangeHandler}/>
+        {text}
+      </header>
+    </div>
+  );
+}
+
+export default App;
